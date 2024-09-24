b498e153105272e47620e9530c2ba1f05d6a6232
Anton Midtsundstad <anton.midtsundstad@gmail.com>
2024-09-25 00:03:19 +0200

    w

--- client.js
+++ client.js
@@ -1,13 +1,26 @@
 'use strict'
-const fs = require('fs');
+const express = require('express')
+const socketio = require('socket.io')
+const cookieParser = require('cookie-parser')
+const jsdom = require('jsdom')
+const fs = require('fs')
+const { dirname } = require('path')
 
-window.addEventListener('load', function() {
-    fs.readFile("login.html", function(err, data){
-        if(err){
-            res.writeHead(404)
-            res.write('ERROR file not found')
-        }else{
-            res.write(data)
-        }
-    })
-    })
+//Sätter igång en express server
+const PORT = 443
+const app = express()
+const expressServer = app.listen(PORT, ()=>{
+    console.log('Min server lyssnar nu på port:' + PORT)
+})
+
+//Middleware
+app.use(cookieParser())
+app.use('/klientgrejer', express.static('./klientgrejer'))
+app.use(express.urlencoded({extended: true}))
+
+//Sätter igång Socketio servern på samma port
+const io = socketio(expressServer)
+
+app.get('/', (req, res)=>{
+    res.sendFile(dirname__ + 'login.html')
+})
