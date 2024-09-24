035e7ed8b63200d1f7137fe19a9b6545d139def8
Anton Midtsundstad <anton.midtsundstad@gmail.com>
2024-09-24 23:50:38 +0200

    w

---
+++ client.js
@@ -0,0 +1,13 @@
+'use strict'
+const fs = require('fs');
+
+window.addEventListener('load', function() {
+    fs.readFile("login.html", function(err, data){
+        if(err){
+            res.writeHead(404)
+            res.write('ERROR file not found')
+        }else{
+            res.write(data)
+        }
+    })
+    })
