4706f8d261b1ddbf2d72346698ba254b7bd1b6f1
Anton Midtsundstad <anton.midtsundstad@gmail.com>
2024-09-24 23:36:49 +0200

    w

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "egenLoginSida")));
+app.listen(443, "Github", ()=>{
+    console.log('Server är igång på github?')
+});
 
 app.get('/', (req, res) =>{
     res.sendFile(__dirname + "login.html");
 } );
 
-app.listen(443, "Github", ()=>{
-    console.log('Server är igång på github?')
-});
